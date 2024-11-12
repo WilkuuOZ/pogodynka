f4897f8cee47fbfa37e9d9fa91dfebf8bc1bbf19
Wilk Hubert <hubertwilk47@gmail.com>
2024-11-12 19:22:05 +0100

    lab-f

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -1,31 +1,24 @@
 <?php
 
-
-
 namespace App\Controller;
 
 use App\Entity\Location;
-use App\Repository\LocationRepository;
-use App\Repository\MeasurementRepository;
+use App\Service\WeatherUtil;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{city}/{countryCode?}', name: 'app_weather')]
-    public function cityWeather(string $city, ?string $countryCode, MeasurementRepository $repository, LocationRepository $locationRepository): Response
+    #[Route('/weather/{country}/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    public function city(
+        #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
+        Location $location,
+        WeatherUtil $util,
+    ): Response
     {
-
-        $location = $locationRepository->findOneBy(['city' => $city, 'country' => $countryCode]);
-
-        if (!$location) {
-            throw $this->createNotFoundException('No location found for city: ' . $city . (isset($countryCode) ? ' in country: ' . $countryCode : ''));
-        }
-
-
-        $measurements = $repository->findBy(['location' => $location]);
-
+        $measurements = $util->getWeatherForLocation($location);
 
         return $this->render('weather/city.html.twig', [
             'location' => $location,
@@ -33,4 +26,3 @@ class WeatherController extends AbstractController
         ]);
     }
 }
-
